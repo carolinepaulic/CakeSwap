6973bc2d0a45df721369ca23ef23afe142a75ce8
Caroline Paulic <caroline.paulic@fire-engine-red.com>
2020-04-25 21:27:00 -0700

    BuilderPanel displays text representations of all layers

--- src/components/BuilderPanel.vue
+++ src/components/BuilderPanel.vue
@@ -1,5 +1,11 @@
 <template>
-    <h2>{{ cake }}</h2>
+    <div>
+        <div v-for="i in cake.numLayers" style="margin-bottom: 20px;">
+            <p>Layer {{ i }}</p>
+            <p>Cake flavor: {{ cake.cakeFlavor }}, Filling: {{ cake.filling || 'none'}}, Frosting: {{ cake.frosting }}</p>
+        </div>
+        <p>Toppings: {{ cake.toppings }} </p>
+    </div>
 </template>
 
 <script>
